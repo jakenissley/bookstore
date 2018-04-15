b3da1da26733bb4f5e1e1ef9ddbb252e4392ea4e
Jake Nissley <jake.nissley@gmail.com>
2018-04-15 10:34:14 -0500

    Item type dropdowns on homepage are now dynamically filled from the database

--- js/home.js
+++ js/home.js
@@ -5,16 +5,24 @@ $('#alert-close').click(function() {
 })
 
 /* Begin Update Item Transaction Code */
-console.log($("#item-selection").val());
-$("#price-increase").on('input', function () {
+function checkUpdateItemInputsFilled() {
     let increase_text = $("#price-increase").val();
+    let selection_text = $("#item-selection").val();
 
-    if (increase_text != "") {
+    if (increase_text != "" && selection_text != "Please Make a Selection") {
         $("#btn-submit-item-update").prop('disabled', false); // Enable the submit button for item-update
     }
     else {
         $("#btn-submit-item-update").prop('disabled', true); // disable the submit button for item-update
     }
+};
+
+$("#price-increase").on('input', function (e) {
+    checkUpdateItemInputsFilled();
+});
+
+$("#item-selection").on('input', function (e) {
+    checkUpdateItemInputsFilled();
 });
 
 function itemIncreaseSubmitClick() {
@@ -28,25 +36,58 @@ function itemIncreaseSubmitClick() {
         let increase_text = increase_double.toString();
         let item_selection = $("#item-selection").val();
 
+        if(item_selection == "Please Make a Selection"){
+            alert("Please select an item type.")
+        }
+        else{
+            let data = { increase: increase_text, item_type: item_selection };
 
-        let data = { increase: increase_text, item_type: item_selection };
-
-        $.ajax({
-            url: "http://localhost:5252/home/updateItemPrice",
-            type: "post",
-            data: JSON.stringify(data),
-            contentType: "application/json",
-            success: function (response) {
-                $('#alert').toggle(500);
-                //alert("Success! Check the 'Item' page to view the new price.");
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown);
-                alert("Unsuccessful.");
-            }
-        });
+            $.ajax({
+                url: "http://localhost:5252/home/updateItemPrice",
+                type: "post",
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                success: function (response) {
+                    $('#alert').toggle(500);
+                    //alert("Success! Check the 'Item' page to view the new price.");
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log(errorThrown);
+                    alert("Unsuccessful.");
+                }
+            });
+        }
     }
-
 }
 $("#btn-submit-item-update").click(itemIncreaseSubmitClick);
+
 /* End Update Item Transaction Code */
+
+/* Build Item Type Dropdowns */
+// AJAX GET to get item types and call buildItemTypeDropdown
+$.get( "http://localhost:5252/home/getItemTypes", function( data ) {
+    data = jQuery.parseJSON(data);
+    let sendData = data.data;
+    let emptyMessage = "Please Make a Selection"
+    buildItemTypeDropdown(sendData, $("#item-selection"), emptyMessage); // build item-selection dropdown
+    buildItemTypeDropdown(sendData, $("#return-item-selection"), emptyMessage); // build return-item-selection dropdown
+});
+
+// Builds dropdown for item type 
+function buildItemTypeDropdown(result, dropdown, emptyMessage)
+{
+    // Remove current options
+    dropdown.html('');
+
+    // Add the empty option with the empty message
+    dropdown.append('<option>' + emptyMessage + '</option>');
+
+    // Check result isnt empty
+    if(result != ''){
+        // Loop through each of the results and append the option to the dropdown
+        $.each(result, function(k, v) {
+            dropdown.append('<option>' + v.Type + '</option>');
+        });
+    }
+}
+/* End Build Item Type Dropdowns */
--- routes/home_routes.js
+++ routes/home_routes.js
@@ -8,7 +8,7 @@ router.post('/updateItemPrice', function (req, res) {
     let item_type = req.body.item_type;
     let increase = req.body.increase;
     
-    if (item_type == "" || increase == "") {
+    if (item_type == "Please Make a Selection" || increase == "") {
         res.status(406).send("Blank/Invalid input.");
     }
     else {
@@ -22,6 +22,28 @@ router.post('/updateItemPrice', function (req, res) {
         });  
     }
 });
+
+// returns list of item types
+router.get('/getItemTypes', function (req, res){
+    const query = "SELECT item.Type FROM item";
+    connection.query(query, function (err, rows, fields) {
+        if (err) {
+            res.status(400).send("home_routes/getItemTypes error: error retrieving item types");
+        } else {
+            if (rows.length > 0) {
+                let returnData = {};
+                returnData['sEcho'] = 1;
+                returnData['iTotalRecords'] = rows.length;
+                returnData['iTotalDisplayRecords'] = rows.length;
+                returnData['data'] = rows;
+                res.send(JSON.stringify(returnData));
+            } else {
+                res.status(204).send("No Content.")
+            }
+        }
+    });
+});
+
 /* End Update Item Transaction Code */
 
 module.exports = router;
